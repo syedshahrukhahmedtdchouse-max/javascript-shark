de13f0c2a0d07b3f42cc2bb2fc5a92133348db4c
syedshahrukhahmedtdchouse-max <syedshahrukhahmedtdchouse>
2026-09-16 04:31:56 -0700

    day17

---
+++ day17/script.js
@@ -0,0 +1,24 @@
+// fetch("https://catfact.ninja/fact")
+// .then((res)=>{
+// return res.json();
+// })
+// .then((data)=>{
+// console.log(data.fact);
+// });
+
+// async function fetchData(url) {
+//     const res = await fetch(url);
+//     const data = await res.json();
+//     return data
+    
+// }
+// const button = document.querySelector("#button")
+// const h4 = document.querySelector("#h4")
+
+// button.addEventListener("click",async()=>{
+//     const data = await fetchData ("https://catfact.ninja/fact");
+//     h4.innerText = data.fact
+// })
+// const button = document.querySelector("#button")
+// const
+
