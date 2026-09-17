c513956e5a49562816aca31964bab6843b2d1d18
syedshahrukhahmedtdchouse-max <syedshahrukhahmedtdchouse>
2026-09-17 03:58:22 -0700

    day17-2

--- day17/script.js
+++ day17/script.js
@@ -22,3 +22,38 @@
 // const button = document.querySelector("#button")
 // const
 
+
+
+async function main(){
+    const response = await fetch ("https://dummyjson.com/todos");
+    const data = await response.json();
+    const todoArr = data?.todos;
+
+    todoArr.forEach((element)=>{
+        const idElem = document.createElement("span");
+        idElem.innerText="ID:"+element?.id;
+
+        const useridElem = document.createElement("span");
+        useridElem.innerText="userid:"+element?.userid;
+
+        const todosElem = document.createElement("h3");
+        todosElem.innerText="Todo:"+element?.todo;
+
+        const compeleteelem = document.createElement("span")
+        compeleteelem.innerText="completed:"+ element?.completed;
+
+        const hr = document
+
+const container = document.createElement("div");
+container.insertAdjacentElement("beforeend",idElem);
+container.insertAdjacentElement("beforeend",useridElem);
+container.insertAdjacentElement("beforeend",todosElem);
+container.insertAdjacentElement("beforeend",compeleteelem);
+
+document.querySelector("body").insertAdjacentElement("beforeend",container)
+
+
+    });
+}
+
+main()
